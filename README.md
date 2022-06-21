146e88e2d114d7fe08122143a6264ae4104ddfaa
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-06-21 14:56:22 +0200

    feat: functions in docker

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nhost functions in Docker
+
+## Features
+- Hot reload of package.json, lock files, and the functions directory
+- Accepts both Javascript and Typesript
+- Create a package.json file if missing
+- Customisable custom package manager (pnpm by default)
+- No need to add the Express dependency anymore
+
+## Example
+
+```sh
+cd example
+cp .env.example .env
+# Full Nhost
+docker-compose up
+
+# Or only functions
+docker-compose up functions traefik
+
+```
---
+++ server.ts
@@ -0,0 +1,29 @@
+import path from 'path'
+
+import express from 'express'
+import glob from 'glob'
+
+const PORT = 3000
+
+const main = async () => {
+  const app = express()
+  const functionsPath = path.join(process.cwd(), 'functions')
+  const files = glob.sync('**/*.@(js|ts)', { cwd: functionsPath })
+
+  for (const file of files) {
+    const { default: handler } = await import(path.join(functionsPath, file))
+    if (handler) {
+      const route = `/${file}`.replace(/(\.ts|\.js)$/, '').replace(/\/index$/, '/')
+      app.all(route, handler)
+      console.log(`Loaded route ${route} from ./functions/${file}`)
+    } else {
+      console.warn(`No default export in ./functions/${file}`)
+    }
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
+}
+
+main()
